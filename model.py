63ab68b92aa60d586bdd1e072b7f0ffc7de1811d
Lindy Blackburn <lindylam@gmail.com>
2017-07-15 16:14:19 -0400

    Disk, Crescent, Ring model, 2nd moment, fixes

--- model.py
+++ model.py
@@ -18,18 +18,28 @@ class model(object):
 
     # initialize model as copy of an existing model, maintain coord
     def __init__(self, other=None):
-        if other is None: # set defaults
-            self.flux = lambda: 1.0
-            self.com = lambda: np.array((0., 0.))
+        # defaults
+        self.flux = lambda: 1.0
+        self.com = lambda: np.array((0., 0.))
+        if other is not None:
+            self.eval = other.eval
+            self.flux = other.flux
+            self.com = other.com
+            self.var = other.var
+            self.pp = other.pp
         self.parent = other # do we need to record keep to preserve model tree?
 
     # operators on model, will return a new transformed model
     def __repr__(self):
         return self.pp()
 
+    # return a copy of self: model(self)
+    def copy(self):
+        return model(self)
+
     # shift model by dx and dy in image domain [radians on sky]
     # note that we never anticipate shifting in uv coords, or mulitplying by complex exp in xy coords
-    def shift(self, dx, dy):
+    def shift(self, dx, dy=0):
         transformed = model(self)
         def eval(r, s, coord='xy'):
             if coord == 'uv':
@@ -38,7 +48,6 @@ class model(object):
                 return self.eval(r-dx, s-dy, coord)
         transformed.eval = eval
         transformed.com = lambda: self.com() + np.array((dx, dy)) # center of mass in x,y
-        transformed.flux = self.flux # total flux
         transformed.pp = lambda: "%s(x-%s, y-%s)" % (self.pp(), str(dx), str(dy))
         return transformed
 
@@ -48,8 +57,6 @@ class model(object):
             hy = hx
         if norm:
             transformed = self.scale(hx, hy).divide(hx*hy)
-            transformed.flux = self.flux
-            transformed.com = self.com
             return transformed
         transformed = model(self)
         def eval(r, s, coord='xy'):
@@ -58,8 +65,8 @@ class model(object):
             else: # xy
                 return self.eval(r/hx, s/hy, coord) # truediv? theano will truediv
         transformed.eval = eval
-        transformed.com = self.com
         transformed.flux = lambda: self.flux() * hx * hy
+        transformed.var = lambda: np.array((hx**2, hy**2)) * self.var()
         transformed.pp = lambda: "%s(x/%s, y/%s)" % (self.pp(), str(hx), str(hy))
         return transformed
 
@@ -73,8 +80,11 @@ class model(object):
         def com():
             (x, y) = self.com()
             return np.array(((cth*x - sth*y), (sth*x + cth*y)))
+        def var(): # guess
+            (x, y) = self.var()
+            return np.array(((cth*x - sth*y), (sth*x + cth*y)))
         transformed.com = com
-        transformed.flux = self.flux
+        transformed.var = var
         transformed.pp = lambda: "R[%s, %s deg]" % (self.pp(), str(theta*180./np.pi))
         return transformed
 
@@ -82,7 +92,6 @@ class model(object):
     def multiply(self, factor):
         transformed = model(self)
         transformed.eval = lambda r,s,coord='xy': factor * self.eval(r, s, coord)
-        transformed.com = self.com
         transformed.flux = lambda: factor * self.flux()
         transformed.pp = lambda: "(%s x %s)" % (str(factor), self.pp())
         return transformed
@@ -91,17 +100,24 @@ class model(object):
     def divide(self, factor):
         transformed = model(self)
         transformed.eval = lambda r,s,coord='xy': self.eval(r, s, coord) / factor
-        transformed.com = self.com
         transformed.flux = lambda: self.flux() / factor
         transformed.pp = lambda: "(%s / %s)" % (self.pp(), str(factor))
         return transformed
 
     # add an additional model to model
+    # calculating moments here is getting inefficient -- better with some memoization scheme
     def add(self, other):
         transformed = model(self)
         transformed.eval = lambda r,s,coord='xy': self.eval(r, s, coord) + other.eval(r, s, coord)
         transformed.com = lambda: (self.flux()*self.com() + other.flux()*other.com()) / (self.flux() + other.flux())
+        def var():
+            (f1, f2) = (self.flux(), other.flux())
+            (c1, c2) = (self.com(), other.com())
+            com = (f1*c1 + f2*c2) / (f1+f2) # new center-of-mass
+            (d1, d2) = (c1-com, c2-com) # difference vector to new center-of-mass
+            return (f1*(self.var() + d1**2) + f2*(other.var() + d2**2)) / (f1+f2)
         transformed.flux = lambda: self.flux() + other.flux()
+        transformed.var = var
         transformed.pp = lambda: "(%s + %s)" % (self.pp(), other.pp())
         return transformed
 
@@ -110,6 +126,7 @@ class model(object):
         transformed = model(self)
         transformed.eval = lambda r,s,coord='xy': self.eval(r, s, coord) - other.eval(r, s, coord)
         transformed.com = lambda: (self.flux()*self.com() - other.flux()*other.com()) / (self.flux() + other.flux())
+        transformed.var = lambda: self.var() + other.var() # too complicated to do right..
         transformed.flux = lambda: self.flux() - other.flux()
         transformed.pp = lambda: "(%s - %s)" % (self.pp(), other.pp())
         return transformed
@@ -142,16 +159,15 @@ class model(object):
                     return ret
         transformed.eval = eval
         transformed.com = lambda: self.com() + other.com()
-        print((repr(self), self.eval, self.com, self.flux, other.flux))
         transformed.flux = lambda: self.flux() * other.flux()
+        transformed.var = lambda: self.var() + other.var()
         transformed.pp = lambda: "(%s o %s)" % (self.pp(), other.pp())
         return transformed
 
-    # center image to (0, 0)
+    # center image to (0, 0): shortcut to shift by -com()
     def center(self):
         transformed = model(self)
         transformed.com = lambda: np.array((0., 0.))
-        transformed.flux = self.flux
         def eval(xx, yy, coord='xy'):
             com = self.com()
             return self.shift(-com[0], -com[1]).eval(xx, yy, coord)
@@ -159,18 +175,31 @@ class model(object):
         transformed.pp = lambda: "Center[%s]" % (self.pp())
         return transformed
 
+    # blur by Gaussian kernel, shortcut to convolve(Gauss.scale(hx, hy).rotate(theta))
+    def blur(self, hx, hy=None, theta=None, deg=False):
+        if hy is None:
+            hy = hx
+        kern = Gauss.scale(hx, hy, norm=True)
+        if theta is None:
+            mod = self.convolve(kern)
+        else:
+            mod = self.convolve(kern.rotate(theta, deg))
+        return mod
+
     # for xy use image size as FOV
     # for uv use size of smallest feature for FOV
-    def show(self, coord='xy', fov=256.):
+    def show(self, coord='xy', fov=None, n=256, cmap='afmhot'):
         import matplotlib.pyplot as plt
+        if fov is None:
+            fov = 3. * np.sqrt(np.max(self.var()))
         if coord == 'uv':
-            r = np.linspace(-2./fov, 2./fov, 256, endpoint=False)
+            r = np.linspace(-2./fov, 2./fov, n, endpoint=False)
         else:
-            r = np.linspace(-fov/2., fov/2., 256, endpoint=False)
+            r = np.linspace(-fov/2., fov/2., n, endpoint=False)
         dr = r[1] - r[0]
         (rr, ss) = np.meshgrid(r, r)
         a = np.abs(self.eval(rr, ss, coord))
-        plt.imshow(a, origin='lower', extent=[-(fov+dr)/2., (fov-dr)/2., -(fov+dr)/2., (fov-dr)/2.])
+        plt.imshow(a, origin='lower', vmin=0, extent=[-(fov+dr)/2., (fov-dr)/2., -(fov+dr)/2., (fov-dr)/2.], cmap=cmap)
         plt.xlabel(coord[0])
         plt.ylabel(coord[1], rotation=0.)
 
@@ -192,23 +221,39 @@ Point = model()
 Point.pp = lambda: "Point"
 # how best to do this and preserve shape independent of data type? u/u? what is norm for xy coords?
 Point.eval = lambda r,s,coord='xy': 1.+0j if coord=='uv' else 1. * ((r==0.) & (s==0.))
+Point.var = lambda: np.array((0., 0.))
 
 # sigma_xy=1, sigma_uv=1/2pi circular gaussian at 0, 0 with total flux = 1
 Gauss = model()
 Gauss.pp = lambda: "Gauss" # sigma_uv = 1/(2pi*sigma_xy)
 Gauss.eval = lambda r,s,coord='xy': (np.exp(-2.*np.pi**2*(r**2 + s**2)) * 1.+0j) if coord=='uv' \
                           else (np.exp(-0.5*(r**2 + s**2)) * (2.*np.pi)**(-1))  # np.sqrt okay theano
+Gauss.var = lambda: np.array((1., 1.))
 
 # r=1 circle with total flux = 1 (uv only)
 Circle = model()
 Circle.pp = lambda: "Circle" # unit circle of unit flux (delta function at r=1 / 2pi)
 Circle.eval = lambda r,s,coord='xy': j0(2*np.pi*(r**2 + s**2)) if coord=='uv' \
                           else (np.sqrt(r**2 + s**2) == 1.) / (2*np.pi)
+Circle.var = lambda: np.array((0.5, 0.5))
 
 # r=1 disk at 0, 0 with total flux = 1
 Disk = model()
 Disk.pp = lambda: "Disk"
 Disk.eval = lambda r,s,coord='xy': np.nan_to_num(j1(2*np.pi*(np.sqrt(r**2 + s**2)))/(np.sqrt(r**2 + s**2)))/np.pi + 1.*(r**2 + s**2 == 0.) if coord=='uv' \
                           else (np.sqrt(r**2 + s**2) < 1) / np.pi # np.sqrt okay theano
+Disk.var = lambda: np.array((1./3., 1./3.))
 
+# Crescent model with radius r1<r2, total flux=1
+# default is to center at middle of outer disk
+def Crescent(r1=0.75, r2=1.0):
+    mod = (Disk.scale(r2) - Disk.scale(r1).shift(r2-r1,0)).divide((r2**2-r1**2))
+    mod.pp = lambda: "Crescent(%s,%s)" % (str(r1), str(r2))
+    return mod
+
+# Ring model with radius r1<r2, total flux=1
+def Ring(r1=0.75, r2=1.0):
+    mod = (Disk.scale(r2) - Disk.scale(r1)).divide((r2**2 - r1**2))
+    mod.pp = lambda: "Ring(%s,%s)" % (str(r1), str(r2))
+    return mod
 
